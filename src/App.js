53aab95e92ec8105196b29c95205529afd30f08c
Partha4real <67099030+Partha4real@users.noreply.github.com>
2021-01-11 14:03:16 +0530

    App setup and Dropdown menu with API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import {FormControl, Select, MenuItem} from '@material-ui/core'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  useEffect(() => {
+    // https://disease.sh/v3/covid-19/countries
+    const getCountriesDAta = async () =>{
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            {
+              name: country.country, // United States, France
+              value: country.countryInfo.iso2, // UK, USA, FR
+              flag: country.countryInfo.flag
+            }
+          ));
+          setCountries(countries);
+        });
+    };
+    getCountriesDAta();
+  }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+    console.log('Yooo>>>', countryCode)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1> 
+        <FormControl className="app__dropdown">   
+          <Select varient="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {/* loop through all the countries and shoe dropdown */}
+            {countries.map(country => (
+              <MenuItem value={country.value}>
+                 <p>{country.name}</p>
+                <img className="app__dropdownFlag" src={country.flag} alt="country-flag" />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div className="app__stats">
+        
+      </div>
     </div>
   );
 }
